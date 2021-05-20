79f56fdc49660cfb98e097f37d616d76c3e62e07
statkowski1 <61691370+statkowski1@users.noreply.github.com>
2021-05-20 20:22:08 +0200

    Add files via upload

---
+++ Zadania/Zadanie1_WYKRES/zad1.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+import seaborn as sns
+
+nazwa = ['A', 'B', 'C', 'D', 'E']
+wartosci = [35, 21, 27, 33, 15]
+
+slownik = {'A': 21, 'B': 27, 'C': 33, 'D': 15, 'E': 35}
+colors = [ '#d82647' ,'#2af6ea','#af768a' ,'#a2d1ef', '#7c1b53']
+plt.figure(figsize=(6, 6))
+plt.pie(slownik.values(), labels=slownik.values(), colors=colors, startangle = 90)
+plt.title('Tytuł')
+plt.legend(slownik.keys(), loc='upper left')
+plt.savefig('obraz.png')
+plt.show()
